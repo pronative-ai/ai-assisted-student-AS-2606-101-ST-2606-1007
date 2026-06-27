d1edfa53b16392d3583d1658d4dc48fa30e963d2
Madan Sudarman <madan@pronative.ai>
2026-06-27 12:21:31 +0530

    Delete dist directory

--- dist/api/routes.d.ts
+++
@@ -1,3 +0,0 @@
-import { Router } from 'express';
-export declare function createRouter(): Router;
-//# sourceMappingURL=routes.d.ts.map
--- dist/config/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { SpApiConfig } from '../types';
-export declare function getSpApiConfig(): SpApiConfig;
-export declare const PORT: number;
-//# sourceMappingURL=index.d.ts.map
